8ca0b00b4e78e212c0e8a308632ca66739c1c5b2
Emre Sapaz <sapazemre@gmail.com>
2025-02-23 18:56:25 +0300

    Manager

---
+++ Manager/Manager.py
@@ -0,0 +1,101 @@
+from tkinter import *
+from tkinter import messagebox
+import random
+import pyperclip
+import json
+
+# ---------------------------- CONSTANTS ---------------------------------------- #
+BG_COLOR = "#D1F8EF"
+BLACK = "#000000"
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+           'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G',
+           'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+def password_generator():
+    password_text.delete(0,END)
+    password_list_letter = [l for _ in range(random.randint(6,8)) for l in random.choice(letters)]
+    password_list_symbol = [l for _ in range(random.randint(2, 4)) for l in random.choice(symbols)]
+    password_list_number = [l for _ in range(random.randint(2, 4)) for l in random.choice(numbers)]
+    password_list = password_list_number + password_list_symbol + password_list_letter
+    random.shuffle(password_list)
+    password = ""
+    for i in password_list:
+        password += i
+    password_text.insert(0,password)
+    pyperclip.copy(password)
+# ---------------------------- SAVE PASSWORD ------------------------------------ #
+def save_password():
+    website = website_text.get()
+    mail = mail_text.get()
+    password = password_text.get()
+    new_data = {
+        website : {
+            "mail" : mail,
+            "password" : password
+        }
+    }
+    if len(website) == 0 or len(mail) == 0 or len(password) == 0:
+        messagebox.showwarning("Empty Space", "Do Not Leave Any Field Empty !")
+    else:
+        confirm = messagebox.askyesno("Confirmation", "Do You Confirm?")
+
+        if confirm:
+            with open("password.json", "r+", encoding="utf-8") as file:
+                data = json.load(file)
+                data.update(new_data)
+            with open("password.json", "w", encoding="utf-8") as file:
+                json.dump(data,file,indent=4)
+                website_text.delete(0, END)
+                mail_text.delete(0,END)
+                password_text.delete(0, END)
+# ---------------------------- UI SETUP ---------------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20, bg=BG_COLOR)
+window.minsize(450, 300)
+
+canvas = Canvas(width=200, height=200, highlightthickness=0, bg=BG_COLOR)
+lock = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=lock)
+canvas.grid(padx=5, pady=5, column=1, row=1, rowspan=3)
+
+# Labels #
+website_label = Label(text="Website                :", fg=BLACK, font="Arial", bg=BG_COLOR)
+website_label.grid(padx=5, pady=5, column=2, row=1)
+
+mail_label = Label(text="Email/Username :", fg=BLACK, font="Arial", bg=BG_COLOR)
+mail_label.grid(padx=5, pady=5, column=2, row=2)
+
+password_label = Label(text="Password            :", fg=BLACK, font="Arial", bg=BG_COLOR)
+password_label.grid(padx=5, pady=5, column=2, row=3)
+
+# Entry #
+website_text = Entry()
+website_text.config(width=60)
+website_text.grid(padx=5, pady=5, column=3, row=1, columnspan=2)
+website_text.focus()
+
+mail_text = Entry()
+mail_text.config(width=60)
+mail_text.grid(padx=5, pady=5, column=3, row=2, columnspan=2)
+
+password_text = Entry()
+password_text.config(width=40)
+password_text.grid(padx=5, pady=5, column=3, row=3)
+
+# Buttons #
+generate_password_button = Button(text="Generate Password", highlightthickness=0,
+                                  command = lambda : password_generator())
+generate_password_button.config(width=15)
+generate_password_button.grid(padx=5, pady=5, column=4, row=3)
+
+add_button = Button(text="Add", highlightthickness=0,
+                    command= lambda : save_password())
+add_button.config(width=52)
+add_button.grid(padx=5, pady=5, column=3, row=5, columnspan=2)
+
+window.mainloop()
